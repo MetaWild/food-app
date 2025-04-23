9183e9d33b6967055557905bc4259db03b866976
Joshua Salas <jsalas1@unm.edu>
2025-04-23 14:26:09 -0600

    CORS policy update

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS for local frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://food-app-zpft.onrender.com"],
+    allow_origins=["http://localhost:5173", "https://food-app-zpft.onrender.com", "http://10.0.0.28:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
