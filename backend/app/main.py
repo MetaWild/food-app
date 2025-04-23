b37c56266e1fb171316215684c6ad9b877011984
Joshua Salas <jsalas1@unm.edu>
2025-04-23 16:36:17 -0600

    Cors policy update

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://food-app-zpft.onrender.com", "http://10.0.0.28:5173"],
+    allow_origins=["http://localhost:5173", "https://food-app-zpft.onrender.com", "http://10.0.0.28:5173", "https://food-app-eta-murex.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
